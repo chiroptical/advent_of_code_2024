634cf78da51054394c8f4d2e2997abf632632a14
Barry Moore <chiroptical@gmail.com>
2024-12-13 17:02:53 -0500

    Finish 2024 day 12 part 1

--- src/solution_day_12_2024.erl
+++ src/solution_day_12_2024.erl
@@ -64,10 +64,9 @@ look(P, Plant, Garden) ->
 get_all(Look, Pos, Plant, Garden, Set) ->
     case Look of
         false ->
-            sets:new();
+            Set;
         true ->
-            Next = sets:add_element(Pos, Set),
-            case sets:is_equal(Set, Next) of
+            case sets:is_element(Pos, Set) of
                 true ->
                     Set;
                 false ->
@@ -87,11 +86,11 @@ find_positions({X, Y}, Plant, Garden, Set) ->
     LookRight = look(Right, Plant, Garden),
 
     UpSet = get_all(LookUp, Up, Plant, Garden, Set),
-    DownSet = get_all(LookDown, Down, Plant, Garden, Set),
-    LeftSet = get_all(LookLeft, Left, Plant, Garden, Set),
-    RightSet = get_all(LookRight, Right, Plant, Garden, Set),
+    DownSet = get_all(LookDown, Down, Plant, Garden, UpSet),
+    LeftSet = get_all(LookLeft, Left, Plant, Garden, DownSet),
+    RightSet = get_all(LookRight, Right, Plant, Garden, LeftSet),
 
-    sets:union([Set, UpSet, DownSet, LeftSet, RightSet]).
+    RightSet.
 
 count_sides({X, Y}, Plant, Garden) ->
     Positions = [{X, Y + 1}, {X, Y - 1}, {X + 1, Y}, {X - 1, Y}],
@@ -102,49 +101,38 @@ count_sides({X, Y}, Plant, Garden) ->
         end,
         Positions
     ),
-    {Matches, 4 - length(Matches)}.
-
-add_matches(Matches, Set) ->
-    lists:foldl(
-        fun(Elem, Acc) ->
-            sets:add_element(Elem, Acc)
-        end,
-        Set,
-        Matches
-    ).
+    4 - length(Matches).
 
 -spec part_one(_) -> integer().
 part_one(Garden) ->
     State =
         lists:foldl(
             fun({Pos, Plant}, State) ->
+                %% logger:notice(#{start => Pos}),
                 case find_group_number(Pos, State#state.positions, State#state.current_group) of
                     {seen, GroupNumber} ->
                         check_plant_matches_group(Plant, GroupNumber, State#state.labels),
-                        {Matches, Sides} = count_sides(Pos, Plant, Garden),
+                        Sides = count_sides(Pos, Plant, Garden),
                         NewState = State#state{
                             sides = maps:update_with(
                                 GroupNumber, fun(X) -> X + Sides end, State#state.sides
                             ),
                             spaces = maps:update_with(
                                 GroupNumber, fun(X) -> X + 1 end, State#state.spaces
-                            ),
-                            positions = maps:update_with(
-                                GroupNumber,
-                                fun(X) -> add_matches(Matches, X) end,
-                                State#state.positions
                             )
                         },
                         NewState;
                     {new, GroupNumber} ->
-                        %% TODO: Use find_positions/4 to update positions with group number
-                        {Matches, Sides} = count_sides(Pos, Plant, Garden),
+                        GroupPositions = find_positions(
+                            Pos, Plant, Garden, sets:from_list([Pos], [{version, 2}])
+                        ),
+                        Sides = count_sides(Pos, Plant, Garden),
                         NewState = State#state{
                             labels = maps:put(GroupNumber, Plant, State#state.labels),
                             sides = maps:put(GroupNumber, Sides, State#state.sides),
                             spaces = maps:put(GroupNumber, 1, State#state.spaces),
                             positions = maps:put(
-                                GroupNumber, sets:from_list([Pos | Matches]), State#state.positions
+                                GroupNumber, GroupPositions, State#state.positions
                             ),
                             current_group = GroupNumber
                         },
@@ -154,16 +142,18 @@ part_one(Garden) ->
             #state{},
             Garden
         ),
-    Positions = State#state.positions,
+    Labels = State#state.labels,
     Sides = State#state.sides,
     Spaces = State#state.spaces,
-    Check = 3,
-    %% logger:notice(#{
-    %%     positions => maps:get(Check, Positions),
-    %%     sides => maps:get(Check, Sides),
-    %%     spaces => maps:get(Check, Spaces)
-    %% }),
-    42.
+    maps:fold(
+        fun(K, _V, Acc) ->
+            Si = maps:get(K, Sides),
+            Sp = maps:get(K, Spaces),
+            Acc + (Si * Sp)
+        end,
+        0,
+        Labels
+    ).
 
 -spec part_two(_) -> integer().
 part_two(_Garden) ->
--- test/solution_day_12_2024_tests.erl
+++ test/solution_day_12_2024_tests.erl
@@ -2,12 +2,20 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(DEBUG, true).
+-define(DEBUG, false).
 
 -if(?DEBUG =:= true).
 -define(LOG(X), logger:notice(X)).
 -else.
 -define(LOG(X), ok).
+-endif.
+
+-define(RUN_SLOW_TESTS, false).
+
+-if(?RUN_SLOW_TESTS =:= true).
+-define(RUN(X), X).
+-else.
+-define(RUN(X), ?_assert(true)).
 -endif.
 
 test_input() ->
@@ -31,7 +39,7 @@ test_input() ->
 %%     ?assertEqual(34, solution_day_12_2024:part_two(Parse)).
 
 %% part_two_solution_test() ->
-%%     {ok, Input} = file:read_file("inputs/2024-day-8.txt"),
+%%     {ok, Input} = file:read_file("inputs/2024-day-12.txt"),
 %%     {ok, Lex} = solution_day_12_2024:lex(binary_to_list(Input)),
 %%     {ok, Parse} = solution_day_12_2024:parse(Lex),
 %%     ?assertEqual(1229, solution_day_12_2024:part_two(Parse)).
@@ -42,13 +50,26 @@ part_one_test() ->
     ?LOG(#{lex => Lex}),
     {ok, Parse} = solution_day_12_2024:parse(Lex),
     ?LOG(#{parse => Parse}),
-    ?assertEqual(14, solution_day_12_2024:part_one(Parse)).
+    ?assertEqual(1930, solution_day_12_2024:part_one(Parse)).
 
-%% part_one_solution_test() ->
-%%     {ok, Input} = file:read_file("inputs/2024-day-8.txt"),
-%%     {ok, Lex} = solution_day_12_2024:lex(binary_to_list(Input)),
-%%     {ok, Parse} = solution_day_12_2024:parse(Lex),
-%%     ?assertEqual(371, solution_day_12_2024:part_one(Parse)).
+%% This is called a "Test Generator", https://learnyousomeerlang.com/eunit
+%% has good information on these. The only thing you need to do is use
+%% ?_assert... instead. Note, this doesn't have _test, we are going to use
+%% it below.
+part_one_solution() ->
+    {ok, Input} = file:read_file("inputs/2024-day-12.txt"),
+    {ok, Lex} = solution_day_12_2024:lex(binary_to_list(Input)),
+    {ok, Parse} = solution_day_12_2024:parse(Lex),
+    ?_assertEqual(1381056, solution_day_12_2024:part_one(Parse)).
+
+part_one_solution_test_() ->
+    {setup, fun() -> ok end, fun(_) ->
+        {
+            timeout,
+            60,
+            [?RUN(part_one_solution())]
+        }
+    end}.
 
 find_group_empty_test() ->
     M = maps:new(),
@@ -69,21 +90,21 @@ find_group_exists_test() ->
 count_sides_empty_test() ->
     M = [{{1, 1}, "X"}],
     ?assertEqual(
-        {[], 4},
+        4,
         solution_day_12_2024:count_sides({1, 1}, "X", M)
     ).
 
 count_sides_one_match_test() ->
     M = [{{1, 1}, "X"}, {{1, 2}, "X"}],
     ?assertEqual(
-        {[{1, 2}], 3},
+        3,
         solution_day_12_2024:count_sides({1, 1}, "X", M)
     ).
 
 count_sides_no_match_test() ->
     M = [{{1, 1}, "X"}, {{1, 2}, "Y"}],
     ?assertEqual(
-        {[], 4},
+        4,
         solution_day_12_2024:count_sides({1, 1}, "X", M)
     ).
 
@@ -136,3 +157,27 @@ find_positions_one_seven_test() ->
         ]
     ),
     ?assert(sets:is_equal(Expected, Result)).
+
+find_positions_group_two_test() ->
+    Input =
+        "WWWWWWEEEEEEEEEEEEEEHHH\n"
+        "WWWWWWEEEEEEEEEEEEHHHHH\n"
+        "WWWMWWEEEEEEEEEEEEHHHHH\n"
+        "WWMMWWEEEEEEEEEEEEEHHHH\n"
+        "MMMWWEEEEEEEEEEMMEHHHHH\n"
+        "MMMEEEEEEEEEEEMMMEMHHHH\n"
+        "MMMEEEEEEEEEMMMMMMMHHHH\n"
+        "MMMMEEEEEEMMMMMMMMMMHHH\n"
+        "MMEEEEEEEEMMMMMMMMMMHHU\n"
+        "MMMEEEEEKEMMMMMMMMMMMHH\n"
+        "MMMEMEEEECMMMMMMMMMMMHH\n"
+        "MMMMMMMMMMMMMMMMMMMUUHU\n",
+    {ok, Lex} = solution_day_12_2024:lex(Input),
+    {ok, Parse} = solution_day_12_2024:parse(Lex),
+    solution_day_12_2024:find_positions(
+        {1, 7},
+        "E",
+        Parse,
+        sets:from_list([{1, 7}], [{version, 2}])
+    ),
+    ok.
