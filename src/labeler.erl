66bc443f33877b0d1e03f1ad85b938a16486df5e
chiroptical <chiroptical@gmail.com>
2024-12-21 20:34:50 -0500

    Speed-up

--- src/labeler.erl
+++ src/labeler.erl
@@ -11,31 +11,40 @@ test_input() ->
     "RRRRI\n"
     "VVRRR\n".
 
+drop_if_exists(Name) ->
+    case ets:whereis(Name) of
+        undefined -> ok;
+        _ -> ets:delete(Name)
+    end.
+
 run_test() ->
     {ok, Lex} = solution_day_12_2024:lex(test_input()),
     {ok, Parse} = solution_day_12_2024:parse(Lex),
-    case ets:whereis(visited) of
-        undefined -> ok;
-        _ -> ets:delete(visited)
-    end,
+    drop_if_exists(visited),
+    drop_if_exists(never_again),
     solve(Parse).
 
 run() ->
     {ok, Input} = file:read_file("inputs/2024-day-12.txt"),
     {ok, Lex} = solution_day_12_2024:lex(binary_to_list(Input)),
     {ok, Parse} = solution_day_12_2024:parse(Lex),
-    case ets:whereis(visited) of
-        undefined -> ok;
-        _ -> ets:delete(visited)
-    end,
+    drop_if_exists(visited),
+    drop_if_exists(never_again),
     T1 = erlang:monotonic_time(millisecond),
     solve(Parse),
     T2 = erlang:monotonic_time(millisecond),
     T2 - T1.
 
+insert_to(never_again, Position) ->
+    ets:insert(never_again, {Position, true});
 insert_to(Visited, Position) ->
     ets:insert(Visited, {Position, true}).
 
+lookup_from(never_again, Position) ->
+    case ets:lookup(never_again, Position) of
+        [] -> false;
+        _ -> true
+    end;
 lookup_from(Visited, Position) ->
     case ets:lookup(Visited, Position) of
         [] -> false;
@@ -43,9 +52,10 @@ lookup_from(Visited, Position) ->
     end.
 
 look(Position, Label, AllPositions) ->
-    Lookup = proplists:lookup(Position, AllPositions),
+    Lookup = maps:get(Position, AllPositions, off_grid),
     case Lookup of
-        {_, A} when A =:= Label -> true;
+        off_grid -> never_again;
+        A when A =:= Label -> true;
         _ -> false
     end.
 
@@ -53,9 +63,12 @@ get_valid_looks({X, Y}, Looks, Visited) ->
     lists:foldl(
         fun({A, B}, Acc) ->
             LookPos = {X + A, Y + B},
-            case lookup_from(Visited, LookPos) of
-                true -> Acc;
-                false -> [{A, B} | Acc]
+            case lookup_from(never_again, LookPos) or lookup_from(Visited, LookPos) of
+                false ->
+                    insert_to(Visited, LookPos),
+                    [{A, B} | Acc];
+                true ->
+                    Acc
             end
         end,
         [],
@@ -68,11 +81,14 @@ find_all_matches(Position = {X, Y}, Label, ValidLooks, AllPositions, Visited) ->
             LookPos = {X + A, Y + B},
             case look(LookPos, Label, AllPositions) of
                 true ->
-                    insert_to(Visited, LookPos),
+                    insert_to(never_again, LookPos),
                     KeepGoing = inner(LookPos, Label, AllPositions, Visited),
-                    s2:union(KeepGoing, s2:insert(LookPos, Acc));
+                    Result = s2:union(KeepGoing, s2:insert(LookPos, Acc)),
+                    Result;
+                never_again ->
+                    insert_to(never_again, LookPos),
+                    Acc;
                 false ->
-                    insert_to(Visited, LookPos),
                     Acc
             end
         end,
@@ -82,6 +98,7 @@ find_all_matches(Position = {X, Y}, Label, ValidLooks, AllPositions, Visited) ->
 
 inner(Position, Label, AllPositions, Visited) ->
     insert_to(Visited, Position),
+    insert_to(never_again, Position),
     Looks = [{0, 1}, {0, -1}, {1, 0}, {-1, 0}],
     ValidLooks = get_valid_looks(Position, Looks, Visited),
     find_all_matches(Position, Label, ValidLooks, AllPositions, Visited).
@@ -95,12 +112,14 @@ inner(Position, Label, AllPositions, Visited) ->
 %% - split the map into one that has the keys I found and one that doesn't
 %% Then just unfold that
 solve(Input) ->
+    Map = maps:from_list(Input),
+    ets:new(never_again, [set, named_table]),
     lists:foldl(
         fun({Key, Val}, {CurrentGroup, Acc}) ->
             case maps:find(Key, Acc) of
                 error ->
                     Visited = ets:new(visited, [set]),
-                    Matches = inner(Key, Val, Input, Visited),
+                    Matches = inner(Key, Val, Map, Visited),
                     NewAcc =
                         s2:fold(
                             fun(Elem, In) ->
